45c1d0e9065d84b4482e9a3e4aa630eb59390b7c
Francis <francisangeles3@hotmail.com>
2016-07-11 21:32:24 -0400

    falta graficar el punto 3 y terminar el 3

--- pyl.py
+++ pyl.py
@@ -11,8 +11,9 @@ prolog.consult("C:/sistema_solar.pl")
 w = Label(master, text="3er Parcial")
 w.pack()
 
+#cuando se selecciona una luna
 def select(event):
-    lunasFrames = Tk()
+    lunasFrames = Toplevel()
     contador = 0
     lunaList = Listbox(lunasFrames)
     e = event.widget
@@ -21,7 +22,7 @@ def select(event):
     for luna in prolog.query("satelites(Planeta,Luna)"):
         if valor == luna["Planeta"]:
             if luna["Luna"] == []:
-                lunaList.insert(contador,"No tiene lunas")
+                lunaList.insert(contador,"No tiene luna(s)")
             else:
                 for moon in luna["Luna"]:
                     lunaList.insert(contador,moon)
@@ -31,7 +32,7 @@ def select(event):
     print 'You selected item %d: "%s"' % (index, valor)
 
 def lunasPlanetas():
-    child1 = Tk()
+    child1 = Toplevel()
     Lb1 = Listbox(child1, selectmode=SINGLE)
 
     i=1
@@ -39,33 +40,83 @@ def lunasPlanetas():
         Lb1.insert(i, luna["Planeta"])
         i += 1
 
-        """if luna["Luna"] == []:
-            print "No hay luna(s) en ", luna["Planeta"]
-            Lb1.insert(i, luna["Planeta"])
-            i+= 1
-        else:
-            for una in luna["Luna"]:
-                print una, "es luna de ", luna["Planeta"]
-                Lb1.insert(i, luna["Planeta"])
-                i += 1"""
-
     Lb1.bind('<<ListboxSelect>>',select)
 
     Lb1.pack()
 
+
+def llamada(masas,diccionario):
+    print masas.split()
+    masaFrame2 = Toplevel()
+    listamasa = Listbox(masaFrame2)
+    k = 0  # contador para listamasa
+    for planeta in masas.split():
+        if planeta in diccionario:
+            listamasa.insert(k, planeta + ": " + str(diccionario[planeta]))
+            k += 1
+        else:
+            listamasa.insert(k, planeta + " no es valido")
+            k += 1
+    listamasa.pack()
+
+
+def dibujar(listaPlaneta):
+    dibujos = Toplevel()
+    listPlan =  listaPlaneta.split()
+    d = Canvas(dibujos)
+    k=0
+
+    for var in prolog.query("planeta(Planeta,Clasificacion,Masa,Posicion)"):
+        for plan in listPlan:
+            if var["Planeta"] == plan:
+                oval = d.create_oval(var["Posicion"]*k,var["Posicion"]+10,var["Masa"]*10,var["Masa"]+10,fill = "blue")
+                #labels = Label(dibujos, text=var["Planeta"])
+                k+5
+    d.pack()
+    #labels.pack(side = TOP)
+
+
+
+def masaPlanetas():
+    masaFrame = Toplevel()
+    masaFrame.minsize(400,400)
+    lista = Listbox(masaFrame)
+    diccionario = {}
+
+    i = 1
+    for luna in prolog.query("planeta(Planeta,Clasificacion,Masa,Posicion)"):
+        lista.insert(i,str(luna["Posicion"])+". "+ luna["Planeta"])
+        diccionario[luna["Planeta"]] = [luna["Masa"]]
+        i += 1
+    lista.pack()
+
+    labels = Label(masaFrame,text="Numero(s) de Planeta(s):")
+    labels.pack()
+    labels.place(relx=.05,rely=.8)
+    listaPlaneta = Entry(masaFrame,bd=15)
+    listaPlaneta.pack(side=BOTTOM)
+    listaPlaneta.place(relx=.5, rely=.82)
+
+    botonMasa = Button(masaFrame, text="Masa", command = lambda: llamada(listaPlaneta.get(),diccionario))
+    botonMasa.pack()
+    botonMasa.place(relx=.06, rely=.87)
+    botonTamano = Button(masaFrame, text="Tamano",command = lambda: dibujar(listaPlaneta.get()))
+    botonTamano.pack()
+    botonTamano.place(relx=.2, rely=.87)
+
+
+
+
 b1 = Button(master,text="Lunas de Planetas",command = lunasPlanetas)
-b2 = Button(master,text="Masa de Planetas")
-b3 = Button(master,text="Tamano de Planetas")
-b4 = Button(master,text="Tiempo respecto al Sol")
+b2 = Button(master,text="Masa de Planetas",command = masaPlanetas)
+b3 = Button(master, text="Tiempo respecto al Sol")
 
 
-b4.pack()
 b3.pack()
 b2.pack()
 b1.pack()
 
-b4.place(relx=.35,rely=.6)
-b3.place(relx=.375,rely=.53)
+b3.place(relx=.35, rely=.53)
 b2.place(relx=.39,rely=.46)
 b1.place(relx=.39,rely=.39)
 
--- tkinter.py
+++ tkinter.py
@@ -1,21 +1,48 @@
-import Tkinter as tk
+from Tkinter import*
 
-class SampleApp(tk.Tk):
-    def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        lb = tk.Listbox(self)
-        lb.insert("end", "one")
-        lb.insert("end", "two")
-        lb.insert("end", "three")
-        lb.bind("<Double-Button-1>", self.OnDouble)
-        lb.pack(side="top", fill="both", expand=True)
+def valueGET(val1, val2):
+    print val1 + "  " + val2
 
-    def OnDouble(self, event):
-        widget = event.widget
-        selection=widget.curselection()
-        value = widget.get(selection[0])
-        print "selection:", selection, ": '%s'" % value
+class ContentUI():
+    def showLogin(self, frame):
 
-if __name__ == "__main__":
-    app = SampleApp()
-    app.mainloop()
+            self.contentUI = ContentUI()
+
+            L1 = Label(frame, text="Name")
+            L1.pack( side = LEFT)
+            L1.grid()
+
+            E1 = Entry(frame, bd =5)
+            E1.pack(side = RIGHT)
+            E1.grid()
+
+            L2 = Label(frame, text="Secret")
+            L2.pack( side = LEFT)
+            L2.grid()
+
+            E2 = Entry(frame, bd =5, show="*")
+            E2.pack(side = RIGHT)
+            E2.grid()
+
+            submit = Button(frame, text="Enter", width=15, command= lambda: valueGET(E1.get(), E2.get()))
+            submit.grid()
+
+class UIDisplay():
+    def play(self):
+        root = Tk()
+
+        root.title("title")
+        #root.geometry(dimension)
+
+        app = Frame(root)
+
+        contentUI = ContentUI()
+        contentUI.showLogin(app)
+
+        app.grid()
+
+
+        root.mainloop()
+
+adkooPlay = UIDisplay()
+adkooPlay.play()
